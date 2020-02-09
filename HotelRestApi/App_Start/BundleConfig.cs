3010494b1130ddd9043b91424cb7fd11d1db777f
bedna <bednarski1978@gmail.com>
2020-02-09 20:52:28 +0100

    Grid View

--- HotelRestApi/App_Start/BundleConfig.cs
+++ HotelRestApi/App_Start/BundleConfig.cs
@@ -15,7 +15,8 @@ namespace HotelRestApi
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-
+            bundles.Add(new ScriptBundle("~/bundles/ext-all.js").Include("~/Scripts/ext-all.js"));
+            bundles.Add(new ScriptBundle("~/bundles/ext-all-debug.js").Include("~/Scripts/ext-all-debug.js"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
